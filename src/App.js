afcfaf85eac15b8872309c1c229161ddc52a0412
ParkSeongju <parksj914@naver.com>
2020-08-30 19:06:54 +0900

    Link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import Home from "./Home";
 import About from "./About";
 
 function App() {
   return (
       <div>
+          <ul>
+              <li>
+                  <Link to="/">홈</Link>
+              </li>
+              <li>
+                  <Link to="/about">소개</Link>
+              </li>
+          </ul>
+          <br />
           <Route path="/" exact={true} component={Home}/>
           <Route path="/about" component={About}/>
       </div>
